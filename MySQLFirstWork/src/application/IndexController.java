cca0e0ee4e3f1388c3e10169f25f30896acd4637
Sebastian Becerra <sbecerraz10@gmail.com>
2019-10-17 16:29:42 -0500

    Almost Everything

--- MySQLFirstWork/src/application/IndexController.java
+++ MySQLFirstWork/src/application/IndexController.java
@@ -1,12 +1,19 @@
 package application;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import data.Actor;
 import data.Genre;
+import data.Movie;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextArea;
@@ -19,14 +26,28 @@ public class IndexController {
 	@FXML private Button btRegisterActor;
 	@FXML private Button btRegisterMovie;
 	@FXML private Button btRegisterGenre;
+	@FXML private Button btConnectMoviesActor;
 	@FXML private Button btSearch;
 	@FXML private TextField tfSearch;
 	@FXML private TextArea taResults;
 	
 	public void initialize() {
-		
+		Platform.runLater( new Thread(()->{
+			searchme();
+		}) );
 	}
 	
+	private void searchme() {
+		tfSearch.setOnKeyReleased((event)->{
+			String data = tfSearch.getText();
+			String actors = Main.getConnection().searchActors(data);
+			String movies = Main.getConnection().searchMovies(data);
+			System.out.println(actors);
+			System.out.println(movies);
+			taResults.setText(actors + "\n" + movies );
+		});
+	}
+
 	@FXML
 	public void registerActor() {
 		System.out.println("Registering Actor");
@@ -35,11 +56,11 @@ public class IndexController {
         dialog.setHeaderText("Please type..");
         DialogPane dialogPane = dialog.getDialogPane();
         dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
-        TextField tfname = new TextField();
-        TextField tfnationality = new TextField();
+        TextField tfname = new TextField(); tfname.setPromptText("Actor's name");
+        TextField tfnationality = new TextField(); tfnationality.setPromptText("Actor's nationality");
         dialog.show();
         dialogPane.setContent(new HBox(14,tfname, tfnationality));
-        Platform.runLater(tfname::requestFocus);
+        //Platform.runLater(tfname::requestFocus);
         dialog.setResultConverter((button) -> {
             if (button == ButtonType.OK) {
                 String name = tfname.getText();
@@ -53,7 +74,35 @@ public class IndexController {
 	
 	@FXML
 	public void registerMovie() {
-		
+		System.out.println("Registering Movie");
+		Dialog<String> dialog = new Dialog();
+		dialog.setTitle("Register Movie");
+        dialog.setHeaderText("Please type..");
+        DialogPane dialogPane = dialog.getDialogPane();
+        dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+        TextField tfname = new TextField(); tfname.setPromptText("Movie's name");
+        TextField tfyear = new TextField(); tfyear.setPromptText("Movie's year");
+        dialog.show();
+        List<String> genres = new ArrayList<String>();
+        HashMap<String,Integer> hashGenres = new HashMap<String,Integer>();
+        for (int i = 0; i < Main.getConnection().getAllGenres().size(); i++) {
+        	String genre =Main.getConnection().getAllGenres().get(i).getGenre();
+        	genres.add(genre);
+        	hashGenres.put(genre,i);
+		}
+        ChoiceBox<String> chbGenre = new ChoiceBox(FXCollections.observableArrayList(genres));
+        dialogPane.setContent(new HBox(20,tfname, tfyear,chbGenre));
+        //Platform.runLater(tfname::requestFocus);
+        dialog.setResultConverter((button) -> {
+            if (button == ButtonType.OK) {
+                String name = tfname.getText();
+                int year = Integer.parseInt(tfyear.getText());
+                String genre =  chbGenre.getSelectionModel().getSelectedItem();
+                Integer genreid = hashGenres.get(genre);
+                Main.getConnection().registerMovie(new Movie(name,year,genreid + 1));
+            }
+            return null;
+        });
 	}
 	
 	@FXML
@@ -64,10 +113,10 @@ public class IndexController {
         dialog.setHeaderText("Please type..");
         DialogPane dialogPane = dialog.getDialogPane();
         dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
-        TextField tfgenre = new TextField();
+        TextField tfgenre = new TextField(); tfgenre.setPromptText("Genre");
         dialog.show();
         dialogPane.setContent(new HBox(14,tfgenre));
-        Platform.runLater(tfgenre::requestFocus);
+        //Platform.runLater(tfgenre::requestFocus);
         dialog.setResultConverter((button) -> {
             if (button == ButtonType.OK) {
                 String genre = tfgenre.getText();
@@ -81,6 +130,49 @@ public class IndexController {
 	public void search() {
 		
 	}
+	
+	@FXML 
+	public void connect() {
+		System.out.println("Connectiong Movie and Actor");
+		Dialog<String> dialog = new Dialog();
+		dialog.setTitle("Movies-Actors");
+        dialog.setHeaderText("Please select..");
+        DialogPane dialogPane = dialog.getDialogPane();
+        dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+        dialog.show();
+        List<String> actors = new ArrayList<String>();
+        List<String> movies = new ArrayList<String>();
+        HashMap<String,Integer> hashActors = new HashMap<String,Integer>();
+        HashMap<String,Integer> hashMovies = new HashMap<String,Integer>();
+        for (int i = 0; i < Main.getConnection().getAllActors().size(); i++) {
+        	String actorname =Main.getConnection().getAllActors().get(i).getName();
+        	actors.add(actorname);
+        	hashActors.put(actorname,i);
+		}
+        for (int i = 0; i < Main.getConnection().getAllMovies().size(); i++) {
+        	String moviename =Main.getConnection().getAllMovies().get(i).getName();
+        	movies.add(moviename);
+        	hashMovies.put(moviename,i);
+        }
+                
+        ChoiceBox<String> chbActors = new ChoiceBox(FXCollections.observableArrayList(actors));
+        ChoiceBox<String> chbMovies = new ChoiceBox(FXCollections.observableArrayList(movies));
+        dialogPane.setContent(new HBox(20,chbActors,chbMovies));
+        //Platform.runLater(tfname::requestFocus);
+        dialog.setResultConverter((button) -> {
+            if (button == ButtonType.OK) {
+                String actorname =  chbActors.getSelectionModel().getSelectedItem();
+                String moviename =  chbMovies.getSelectionModel().getSelectedItem();
+                Integer actorid = hashActors.get(actorname);
+                Integer movieid = hashMovies.get(moviename);
+                Main.getConnection().connectMoviesActors(movieid + 1, actorid + 1);
+                
+            }
+            return null;
+        });
+	}
+	
+	
 	
 	
 }
--- MySQLFirstWork/src/data/MySQLConnection.java
+++ MySQLFirstWork/src/data/MySQLConnection.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MySQLConnection {
 	
@@ -20,6 +21,8 @@ public class MySQLConnection {
 	public static String TABLE_GENRES = "genres";
 	public static String TABLE_ACTORS_MOVIES = "actors_movies";
 	public static String ACTORS_ID = "id";
+	public static String ACTOR_ID = "actorID";
+	public static String MOVIE_ID = "movieID";
 	public static String MOVIES_ID = "id";
 	public static String GENRES_ID = "id";
 	public static String GENRES_GENRE = "genre";
@@ -133,6 +136,125 @@ public class MySQLConnection {
 		return output;
 	}
 	
+	
+	public List<Genre> getAllGenres(){
+		ArrayList<Genre> genres = new ArrayList<Genre>();
+		String sql = "SELECT * FROM $TABLE";
+		sql = sql.replace("$TABLE", TABLE_GENRES);
+		
+		try {
+			ResultSet resultados = statement.executeQuery(sql);
+			
+			while(resultados.next()) {
+				int id = resultados.getInt(1);
+				String genre= resultados.getString(2);
+				
+				genres.add(new Genre(id,genre));
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return genres;
+	}
+	
+	
+	public List<Movie> getAllMovies(){
+		ArrayList<Movie> movies = new ArrayList<Movie>();
+		String sql = "SELECT * FROM $TABLE";
+		sql = sql.replace("$TABLE", TABLE_MOVIES);
+		
+		try {
+			ResultSet resultados = statement.executeQuery(sql);
+			
+			while(resultados.next()) {
+				int id = resultados.getInt(1);
+				String name= resultados.getString(2);
+				int year = resultados.getInt(3);
+				int genreID = resultados.getInt(4);
+				movies.add(new Movie(id, name, year, genreID));
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return movies;
+	}
+	
+	
+	
+	
+	public String searchMovies(String data) {
+		String result = "MOVIES \n";
+		
+		String sql = "SELECT name FROM $TABLE WHERE movies.name LIKE '$name%'";
+		sql = sql.replace("$TABLE", TABLE_MOVIES);
+		sql = sql.replace("$name", data);
+		
+		try {
+			ResultSet resultados = statement.executeQuery(sql);
+			
+			while(resultados.next()) {
+				result += resultados.getString(1) + "\n";
+				
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public String searchActors(String data) {
+		String result = "ACTORS \n";
+		
+		String sql = "SELECT name FROM $TABLE WHERE actors.name LIKE '$name%'";
+		sql = sql.replace("$TABLE", TABLE_ACTORS);
+		sql = sql.replace("$name", data);
+		
+		try {
+			ResultSet resultados = statement.executeQuery(sql);
+			
+			while(resultados.next()) {
+				result += resultados.getString(1) + "\n";
+				
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public void connectMoviesActors(int movieid, int actorid) {
+		System.out.println("Movie id " +movieid); 
+		System.out.println("Actor id" + actorid);
+		String sql="INSERT INTO $TABLE($ACTORID,$MOVIEID) VALUES($VACTORID,$VMOVIEID)";
+		sql=sql
+				.replace("$TABLE",TABLE_ACTORS_MOVIES)
+				.replace("$ACTORID", ACTOR_ID)
+				.replace("$MOVIEID", MOVIE_ID)
+				.replace("$VACTORID",actorid + "" )
+				.replace("$VMOVIEID",movieid + "");
+				
+		
+		try {
+			statement.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	
 
